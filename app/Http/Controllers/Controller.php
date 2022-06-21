f8b33ea99660c38783949eb9947f0ea9735e001e
alhamodi65 <91828081+alhamodi65@users.noreply.github.com>
2022-06-21 02:32:19 +0200

    initial

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -25,19 +25,19 @@ class Controller extends BaseController
 
 
     public function get_validations($type="file"){
-        if($type=="file") 
+        if($type=="file")
             return "3gp,7z,7zip,ai,apk,avi,bin,bmp,bz2,css,csv,doc,docx,egg,flv,gif,gz,h264,htm,html,ia,icns,ico,jpeg,jpg,m4v,markdown,md,mdb,mkv,mov,mp3,mp4,mpa,mpeg,mpg,mpga,octet-stream,odp,ods,odt,ogg,otf,pak,pdf,pea,png,pps,ppt,pptx,psd,rar,rm,rss,rtf,s7z,sql,svg,tar,targz,tbz2,tex,tgz,tif,tiff,tlz,ttf,vob,wav,webm,wma,wmv,xhtml,xlr,xls,xlsx,xml,z,zip,zipx,gif,png,jpeg,qt";
         else if($type=="image")
             return "jpeg,bmp,png,gif";
     }
 
     public function store_file($options)
-    {   
+    {
         $validation=Validator::make($options, [ 'source' => "required|mimes:".$this->get_validations($options["validation"])."|max:250000"]);  if($validation->fails()) {
             $file=$options['source'];
             $filename='';
             return [
-                'success' => false, 
+                'success' => false,
                 'filename' => $options['source'],
 
                 // for new extension
@@ -70,7 +70,7 @@ class Controller extends BaseController
         $path_small = $options["path_to_save"] . $options["small_path"];
         if (null != $options["resize"]) {
             $filename = $user_id . '_' . uniqid() . '_' . time() . '.jpg';
-            
+
             $manager = new ImageManager(['driver' => 'gd']);
             $image = $manager->make($file);
             $back = $manager->canvas($image->width(), $image->height(), '#ffffff');
@@ -100,21 +100,21 @@ class Controller extends BaseController
             $img->stream();
 
 
-            if(isset($options['compress']) && $options['compress']!="-1"){
-                $quality=100;
+            // if(isset($options['compress']) && $options['compress']!="-1"){
+            //     $quality=100;
 
-                if($image2->width()>=3000)
-                    $quality=30;
-                else if ($image2->width()<3000 && $image2->width()>=2000)
-                    $quality=80;
-                else if ($image2->width()<2000 && $image2->width()>=1000)
-                    $quality=90;
+            //     if($image2->width()>=3000)
+            //         $quality=30;
+            //     else if ($image2->width()<3000 && $image2->width()>=2000)
+            //         $quality=80;
+            //     else if ($image2->width()<2000 && $image2->width()>=1000)
+            //         $quality=90;
 
-                $imagick = new \Imagick();
-                $imagick->readImageBlob($img);
-                $imagick->setImageCompressionQuality($quality);
-                $img = $imagick->getImageBlob();
-            }
+            //     $imagick = new Imagick();
+            //     $imagick->readImageBlob($img);
+            //     $imagick->setImageCompressionQuality($quality);
+            //     $img = $imagick->getImageBlob();
+            // }
             try{
                 \Storage::disk($options["file_system_type"])->put($path_small . $filename, $sm_img, $filename);
             }catch(\Exception $e){}
@@ -124,7 +124,7 @@ class Controller extends BaseController
 
         } else {
             $filename = $user_id . '_' . uniqid() . '_' . time() . '.' . $file->getClientOriginalExtension();
-            try{ 
+            try{
                 \Storage::disk($options["file_system_type"])->put($path . $filename, \File::get($file), $filename);
             }catch(\Exception $e){}
         }
@@ -142,7 +142,7 @@ class Controller extends BaseController
             ]
         );
         return [
-            'success' => true, 
+            'success' => true,
             'filename' => $filename,
 
             // for new extension
@@ -167,7 +167,7 @@ class Controller extends BaseController
                 ]
             ]
         ];
- 
+
      }
     public function remove_hub_file($name)
     {
@@ -181,12 +181,12 @@ class Controller extends BaseController
             if($get_file['type']=="project"){
                 $project=\App\Project::where('id',$get_file['type_id'])->firstOrFail();
                 if(!$project->editable())return 0;
-            } 
+            }
             if($get_file['type']=="message" && $get_file['used_at']!=null ){
                 return 0;
-            } 
-            
-            $get_file->delete(); 
+            }
+
+            $get_file->delete();
             return ['success' => true, 'filename' => $name];
         }
         return ['success' => false, 'filename' => $name];
@@ -220,11 +220,11 @@ class Controller extends BaseController
         );
     }
     public static function generate_slug($string){
-        $t = $string; 
+        $t = $string;
         $specChars = array(
             ' ' => '-',    '!' => '',    '"' => '',
             '#' => '',    '$' => '',    '%' => '',
-            '&amp;' => '','&nbsp;' => '', 
+            '&amp;' => '','&nbsp;' => '',
             '\'' => '',   '(' => '',
             ')' => '',    '*' => '',    '+' => '',
             ',' => '',    '₹' => '',    '.' => '',
@@ -235,13 +235,13 @@ class Controller extends BaseController
             '_' => '',    '`' => '',    '{' => '',
             '|' => '',    '}' => '',    '~' => '',
             '-----' => '-',    '----' => '-',    '---' => '-',
-            '/' => '',    '--' => '-',   '/_' => '-',    
-        ); 
+            '/' => '',    '--' => '-',   '/_' => '-',
+        );
         foreach ($specChars as $k => $v) {
             $t = str_replace($k, $v, $t);
         }
- 
+
         return substr($t,0,230);
     }
-    
+
 }
